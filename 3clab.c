e082c15c85e2b1dc35eb24c59ee6a4c84a11f981
pepper236723 <sashakoretsua@gmail.com>
2020-03-20 00:20:10 +0200

    create 3 lab

---
+++ 3clab.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void n_func(char word[])
+{
+    for (int i = 0; i < strlen(word); i++)
+    {
+        if (&word[i] != strrchr(word, word[i]))
+        {
+            printf("|_____|\n");
+            break;
+        }
+        else if (i == (strlen(word) - 1))
+        {
+            printf("%s\n", word);
+        }
+    }
+}
+
+void main()
+{
+    char word[] = "q qwe swad  zcxc qwz";
+    char s_t_a[128] = {};
+    int k = 0;
+    for (int i = 0; i <= strlen(word); i++)
+    {
+        if (word[i] != ' ' && word[i] != '\0')
+        {
+            s_t_a[k] = word[i];
+            k++;
+        }
+        else
+        {
+            n_func(s_t_a);
+            k = 0;
+            memset(s_t_a, 0, sizeof(s_t_a));
+        }
+    }
+}
